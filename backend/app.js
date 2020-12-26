720f12cb227ec006914c68e2b176988da5c6aa56
Florian SIMUNEK <florian.simunek@gmail.com>
2020-12-26 15:32:14 +0100

    Added CRUD

--- backend/app.js
+++ backend/app.js
@@ -1,42 +1,58 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Sauce = require('./models/Sauce');
+
+mongoose.connect('mongodb+srv://Florian:9zN3zDgKyNtR@cluster0.puqcd.mongodb.net/<dbname>?retryWrites=true&w=majority',
+    { useNewUrlParser: true,
+    useUnifiedTopology: true })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 const app = express();
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  next();
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
 });
 
 app.use(bodyParser.json());
 
 app.post('/api/sauces', (req, res, next) =>{
-    console.log(req.body);
-    res.status(201).json({
-        message: "Objet créé !"
+    delete req.body._id;
+    const sauce = new Sauce({
+        ...req.body
     });
+    sauce.save()
+        .then(() => res.status(201).json({message: 'Objet enregistré !'}))
+        .catch(error => res.status(400).json({error}));
 });
 
-app.use('/api/sauces', (req, res, next) => {
-  const sauces = [
-    {
-      id: 'oeihfzeoi',
-      userId: 'qsomihvqios',
-      name: 'test',
-      manufacturer: 'ssss',
-      description: 'Ceci est une sauce',
-      mainPepper: 'Piquante',
-      imageUrl: 'https://pixabay.com/fr/photos/poivrons-l%C3%A9gumes-alimentaire-421087/',
-      heat: 2,
-      likes: 12,
-      dislikes: 4,
-      usersLiked: 'testUser',
-      usersDisliked: 'testUser2'
-    }
-  ];
-  res.status(200).json(sauces);
+app.put('/api/sauces/:id', (req, res, next) => {
+    Sauce.updateOne({_id: req.params.id}, {...req.body, _id: req.params.id})
+        .then(() => res.status(200).json({message: 'Objet modifié !'}))
+        .catch(error => res.status(400).json({error}));
+});
+
+app.delete('/api/sauces/:id', (req, res, next) => {
+    Sauce.deleteOne({_id: req.params.id})
+        .then(() => res.status(200).json({message: 'Objet supprimé !'}))
+        .catch(error => res.status(400).json({error}));
+});
+
+app.get('/api/sauces/:id', (req, res, next) => {
+    Sauce.findOne({_id: req.params.id})
+        .then(sauce => res.status(200).json(sauce))
+        .catch(error => res.status(404).json({error}));
+});
+
+app.get('/api/sauces', (req, res, next) => {
+    Sauce.find()
+        .then(sauces => res.status(200).json(sauces))
+        .catch(error => res.status(400).json({error}));
 });
 
 module.exports = app;
