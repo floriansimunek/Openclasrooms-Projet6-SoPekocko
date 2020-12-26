94aa430223f16ecf29cf15c47944406191f352fd
Florian SIMUNEK <florian.simunek@gmail.com>
2020-12-26 15:37:45 +0100

    Added sauces routes

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-const Sauce = require('./models/Sauce');
+const saucesRoutes = require('./routes/sauces'); 
 
 mongoose.connect('mongodb+srv://Florian:9zN3zDgKyNtR@cluster0.puqcd.mongodb.net/<dbname>?retryWrites=true&w=majority',
     { useNewUrlParser: true,
@@ -21,38 +21,6 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 
-app.post('/api/sauces', (req, res, next) =>{
-    delete req.body._id;
-    const sauce = new Sauce({
-        ...req.body
-    });
-    sauce.save()
-        .then(() => res.status(201).json({message: 'Objet enregistré !'}))
-        .catch(error => res.status(400).json({error}));
-});
-
-app.put('/api/sauces/:id', (req, res, next) => {
-    Sauce.updateOne({_id: req.params.id}, {...req.body, _id: req.params.id})
-        .then(() => res.status(200).json({message: 'Objet modifié !'}))
-        .catch(error => res.status(400).json({error}));
-});
-
-app.delete('/api/sauces/:id', (req, res, next) => {
-    Sauce.deleteOne({_id: req.params.id})
-        .then(() => res.status(200).json({message: 'Objet supprimé !'}))
-        .catch(error => res.status(400).json({error}));
-});
-
-app.get('/api/sauces/:id', (req, res, next) => {
-    Sauce.findOne({_id: req.params.id})
-        .then(sauce => res.status(200).json(sauce))
-        .catch(error => res.status(404).json({error}));
-});
-
-app.get('/api/sauces', (req, res, next) => {
-    Sauce.find()
-        .then(sauces => res.status(200).json(sauces))
-        .catch(error => res.status(400).json({error}));
-});
+app.use('/api/sauces', saucesRoutes);
 
 module.exports = app;
