6f20ee3cb6b9fbf14534f0904f08838f834c5923
Florian SIMUNEK <florian.simunek@gmail.com>
2021-01-26 13:35:05 +0100

    Added comments

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,7 @@ const path = require('path');
 const saucesRoutes = require('./routes/sauces');
 const userRoutes = require('./routes/user');
 
+//Connexion à la Base de données
 mongoose.connect('mongodb+srv://'+process.env.MONGODB_USERNAME+':'+process.env.MONGODB_PASSWORD+'@cluster0.puqcd.mongodb.net/<dbname>?retryWrites=true&w=majority',
     { useNewUrlParser: true,
     useUnifiedTopology: true })
@@ -16,6 +17,7 @@ mongoose.connect('mongodb+srv://'+process.env.MONGODB_USERNAME+':'+process.env.M
 
 const app = express();
 
+//Possibilité de "connexion" entre les deux localhost 3000 et 4200
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -27,6 +29,7 @@ app.use(bodyParser.json());
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
+//Définition des routes
 app.use('/api/sauces', saucesRoutes);
 app.use('/api/auth', userRoutes);
 
--- backend/server.js
+++ backend/server.js
@@ -2,46 +2,50 @@ const http = require('http');
 const app = require('./app');
 
 const normalizePort = val => {
-  const port = parseInt(val, 10);
+    const port = parseInt(val, 10);
 
-  if (isNaN(port)) {
-    return val;
-  }
-  if (port >= 0) {
-    return port;
-  }
-  return false;
+    if (isNaN(port)) {
+        return val;
+    }
+    if (port >= 0) {
+        return port;
+    }
+    return false;
 };
-const port = normalizePort(process.env.PORT || '3000');
+
+//renvoie un port valide
+const port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
+//récupère les différentes erreurs
 const errorHandler = error => {
-  if (error.syscall !== 'listen') {
-    throw error;
-  }
-  const address = server.address();
-  const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
-  switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges.');
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use.');
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges.');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use.');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
 };
 
 const server = http.createServer(app);
 
 server.on('error', errorHandler);
+//écoute d'évenement sur le port sur lequel le serveur s'exécute
 server.on('listening', () => {
-  const address = server.address();
-  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
-  console.log('Listening on ' + bind);
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+    console.log('Listening on ' + bind);
 });
 
 server.listen(port);
